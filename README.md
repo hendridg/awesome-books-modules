4e908228c072df32df2df581d224f928d1aac492
hendridg <hendridg@gmail.com>
2022-01-10 15:24:22 -0400

    Inital commit

---
+++ README.md
@@ -0,0 +1,45 @@
+Awesome Books to course
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Awesome Books Modules
+
+> Awesome books using HTML, CSS, and JavaScripts
+
+![screenshot](./assets/app_screenshot_1.png)
+
+## Built With
+
+- HTML, CSS and JavaScripts
+
+## Getting Started
+
+To get a local copy up follow these simple example steps.
+
+### Prerequisites
+
+- GIT
+
+### Setup
+
+Open a terminal or a git bash in the desired directory and run `https://github.com/hendridg/awesome-books.git`
+
+👤 **Author1**
+
+- GitHub: [@hendridg](https://github.com/hendridg)
+- Twitter: [@hendridg](https://twitter.com/hendridg)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/hendridg)
+
+👤 **Author2**
+
+- GitHub: [@andres-condezo](https://github.com/andres-condezo)
+- Twitter: [@andres_condezo](https://twitter.com/andres_condezo)
+- LinkedIn: [andres-condezo](https://linkedin.com/in/andres-condezo)
+
+👤 **Author3**
+
+- GitHub: [@lisandroseia](https://github.com/lisandroseia)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/lisandro-seia-295120225/)
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ index.js
@@ -0,0 +1,105 @@
+import { DateTime } from 'luxon';
+const listBooks = document.querySelector('.list-books');
+const form = document.querySelector('.form-input');
+const [title, author] = form.elements;
+const [navList, navAdd, navContact] = document.querySelectorAll('.list-item');
+const allBooks = document.querySelector('.all-books');
+const addBook = document.querySelector('.add-book');
+const contact = document.querySelector('.contact');
+
+now = DateTime.now();
+later = DateTime.local(2020, 10, 12);
+i = Interval.fromDateTimes(now, later);
+
+i.length(); //=> 97098768468
+i.length('years'); //=> 3.0762420239726027
+i.contains(DateTime.local(2019)); //=> true
+
+const inputBook = {};
+let books = new Array([]);
+
+if (localStorage.savedBooks) {
+  books = JSON.parse(localStorage.getItem('savedBooks'));
+}
+
+navList.addEventListener('click', () => {
+  allBooks.classList.remove('hidden');
+  addBook.classList.add('hidden');
+  contact.classList.add('hidden');
+});
+
+navAdd.addEventListener('click', () => {
+  addBook.classList.remove('hidden');
+  allBooks.classList.add('hidden');
+  contact.classList.add('hidden');
+});
+
+navContact.addEventListener('click', () => {
+  contact.classList.remove('hidden');
+  allBooks.classList.add('hidden');
+  addBook.classList.add('hidden');
+});
+
+title.addEventListener('change', () => {
+  inputBook.title = title.value;
+});
+
+author.addEventListener('change', () => {
+  inputBook.author = author.value;
+});
+
+const populateFields = () => {
+  localStorage.setItem('savedBooks', JSON.stringify(books));
+};
+
+const Book = class {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  static removeBook(book) {
+    const result = books.filter((b) => b !== book);
+    books = result;
+    populateFields();
+  }
+
+  static addBook = (newBook) => {
+    books.push(newBook);
+    populateFields();
+    this.displayBooks();
+  };
+
+  static displayBooks = () => {
+    listBooks.innerHTML = '';
+    books.map((book) => {
+      const bookDiv = document.createElement('tr');
+      const elementBook = document.createElement('td');
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = 'Remove';
+
+      elementBook.textContent = `"${book.title}" by ${book.author}`;
+
+      bookDiv.classList.add('book-container');
+      bookDiv.appendChild(elementBook);
+      bookDiv.appendChild(deleteBtn);
+
+      listBooks.appendChild(bookDiv);
+
+      deleteBtn.addEventListener('click', () => {
+        this.removeBook(book);
+        listBooks.removeChild(bookDiv);
+      });
+      return listBooks;
+    });
+  };
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  Book.addBook(new Book(inputBook.title, inputBook.author));
+  form.submit();
+});
+
+Book.displayBooks();
+populateFields();
